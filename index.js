8ffd8f15dd8f174817ba3e57a897879f0bc4c9d1
Ana Clara <anasmatos28@gmail.com>
2022-04-20 14:38:36 -0300

    aplicação de feriados

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+
+const holidays = [
+    { date: "1/1/2022", name: "Confraternização mundial" },
+    { date: "1/3/2022", name: "Carnaval" },
+    { date: "4/17/2022", name: "Páscoa" },
+    { date: "4/21/2022", name: "Tiradentes" },
+    { date: "5/1/2022", name: "Dia do trabalho" },
+    { date: "6/16/2022", name: "Corpus Christi" },
+    { date: "9/7/2022", name: "Independência do Brasil" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+    { date: "11/2/2022", name: "Finados" },
+    { date: "11/15/2022", name: "Proclamação da República" },
+    { date: "12/25/2022", name: "Natal" }
+];
+
+app.use(cors())
+
+app.get('/holidays', (req, res) => {
+    res.send(holidays)
+})
+
+app.get('/is-today-holiday', (req, res) => {
+    const hoje = new Date();
+    const hojeFormatado = hoje.toLocaleDateString();
+    const ehFeriado = holidays.find(holiday => holiday.date === hojeFormatado);
+    if (ehFeriado){
+        res.send("Sim, hoje é nome-do-feriado")
+    } else {
+        res.send("Não, hoje não é feriado")
+    }
+})
+
+
+app.listen(4000)
